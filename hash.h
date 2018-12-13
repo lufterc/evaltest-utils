00bec1ac6a7c97adcee79e4ea38262b9086f5c08
l4 <l4>
2018-12-13 16:41:58 +0300

    Futhermore reduce symbol count

--- hash.h
+++ hash.h
@@ -13,30 +13,30 @@ template <typename K, typename V, Hash TSIZE = 1024,
           typename F = KeyHash<K, TSIZE>>
 class HashMap {
 public:
-    struct HashNode {
-        HashNode(K key, V val) : key(key), val(val), next(0) {}
-        K key; V val; HashNode *next;
+    struct Node {
+        Node(K key, V val) : key(key), val(val), next(0) {}
+        K key; V val; Node *next;
     };
 
     HashMap() {
-        table = new HashNode *[TSIZE]();                    // construct zero initialized hash table of size
+        table = new Node *[TSIZE]();                        // construct zero initialized hash table of size
     }
 
     ~HashMap() {
         for (Hash i = 0; i < TSIZE; ++i) {                  // destroy all buckets one by one
-            HashNode *n = table[i];
+            Node *n = table[i];
             while (n != 0) {
-                HashNode *prev = n;
+                Node *p = n;
                 n = n->next;
-                delete prev;
+                delete p;
             }
             table[i] = 0;
         }
         delete [] table;                                    // destroy the hash table
     }
 
-    HashNode* get(const K &key) {
-        HashNode *n = table[hashFunc(key)];
+    Node* get(const K &key) {
+        Node *n = table[hashFunc(key)];
         while (n != 0) {
             if (n->key == key) { return n; }
             n = n->next;
@@ -46,37 +46,37 @@ public:
 
     void put(const K &key, const V &val) {
         Hash hashValue = hashFunc(key);
-        HashNode *prev = 0;
-        HashNode *n = table[hashValue];
+        Node *p = 0;
+        Node *n = table[hashValue];
 
         while (n != 0 && n->key != key) {
-            prev = n;
+            p = n;
             n = n->next;
         }
 
         if (n == 0) {
-            n = new HashNode(key, val);
-            if (prev == 0) { table[hashValue] = n; }        // insert as first bucket
-            else { prev->next = n; }
+            n = new Node(key, val);
+            if (p == 0) { table[hashValue] = n; }           // insert as first bucket
+            else { p->next = n; }
         }
         else { n->val = val; }                              // just update the value
     }
 
     void remove(const K &key) {
         Hash hashValue = hashFunc(key);
-        HashNode *prev = 0;
-        HashNode *n = table[hashValue];
+        Node *p = 0;
+        Node *n = table[hashValue];
 
-        while (n != 0 && n->next != key) { prev = n; n = n->next; }
+        while (n != 0 && n->next != key) { p = n; n = n->next; }
 
         if (n == 0) { return; }                             // key not found
         else {
-            if (prev == 0) { table[hashValue] = n->next; }  // remove first bucket of the list
-            else { prev->next = n->next; }
+            if (p == 0) { table[hashValue] = n->next; }     // remove first bucket of the list
+            else { p->next = n->next; }
             delete n;
         }
     }
 
 private:
-    HashNode **table; F hashFunc;
+    Node **table; F hashFunc;
 };
